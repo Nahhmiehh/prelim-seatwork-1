80e99103af8fff39fcddd0d5cdb332dc63bcb0db
Reynamie Borres <95603-22266142@SERVER2.DOMAIN>
2024-08-19 18:27:43 +0800

    First Commit

---
+++ index.js
@@ -0,0 +1,15 @@
+const user ={
+    fname: "Jessica",
+    lname: "Cordero",
+    gender: "Female",
+    age: 30
+}
+let value1 = ""
+let value2 = ""
+let value3 = ""
+
+//bracket notation
+value1 = user['fname']
+value2 = user['lname']
+value3 = user['age']
+console.log("I am "+ value1 +" "+ value2 + " and I am "+ value3 +" yrs old")
